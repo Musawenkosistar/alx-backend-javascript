3a9d3ef0be5dbcce2ecccdc8c045dcb55efcf1c0
Musawenkosistar <musawenkosi2504@gmail.com>
2024-10-09 03:13:02 +0000

    0x03-ES6_data_manipulation

--- 0x03-ES6_data_manipulation/100-main.js
+++ 0x03-ES6_data_manipulation/100-main.js
@@ -1,15 +1,17 @@
 import { queryAPI, weakMap } from "./100-weak.js";
 
 const endpoint = { protocol: 'http', name: 'getUsers' };
-weakMap.get(endpoint);
 
-queryAPI(endpoint);
-console.log(weakMap.get(endpoint));
-
-queryAPI(endpoint);
-console.log(weakMap.get(endpoint));
-
-queryAPI(endpoint);
-queryAPI(endpoint);
-queryAPI(endpoint);
-queryAPI(endpoint);
+try {
+    queryAPI(endpoint);
+    console.log(weakMap.get(`${endpoint.protocol}://${endpoint.name}`)); // Should print 1
+    queryAPI(endpoint);
+    console.log(weakMap.get(`${endpoint.protocol}://${endpoint.name}`)); // Should print 2
+    queryAPI(endpoint);
+    console.log(weakMap.get(`${endpoint.protocol}://${endpoint.name}`)); // Should print 3
+    queryAPI(endpoint);
+    console.log(weakMap.get(`${endpoint.protocol}://${endpoint.name}`)); // Should print 4
+    queryAPI(endpoint); // Should throw an error
+} catch (error) {
+    console.error(error.message);
+}
